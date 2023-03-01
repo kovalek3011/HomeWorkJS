2a2ea4fdf18ee73b14724a7a811d1ae5910ac19d
kovalek3011 <mrkv3011@gmail.com>
2023-03-01 23:12:46 +0400

    const BigNumber

--- HomeWork/2/module.js
+++ HomeWork/2/module.js
@@ -1,36 +1,37 @@
 /* 2.Написать модуль, который способен выполнять операции с числами любой длины.
 4 метода для сложения, умножения, вычитания и деления. */
-function sumBI(a, b) {
-  res = BigInt(a) + BigInt(b);
-  console.log(res);
-  return res;
-}
-
-function multBI(a, b) {
-  res = BigInt(a) * BigInt(b);
-  console.log(res);
-  return res;
-}
-
-function subBI(a, b) {
-  res = BigInt(a) - BigInt(b);
-  console.log(res);
-  return res;
-}
-
-function divBI(a, b) {
-  if (b == 0) {
-    console.log("На ноль делить нельзя");
-    return "На ноль делить нельзя";
-  } else {
-    res = BigInt(a) / BigInt(b);
+const BigNumber = {
+  add: (a, b) => {
+    res = BigInt(a) + BigInt(b);
     console.log(res);
     return res;
-  }
-}
+  },
 
-alert(sumBI(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
-alert(multBI(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
-alert(subBI(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
-alert(divBI(Number.MAX_SAFE_INTEGER, 0));
-alert(divBI(Number.MAX_SAFE_INTEGER, 2));
+  multiply: (a, b) => {
+    res = BigInt(a) * BigInt(b);
+    console.log(res);
+    return res;
+  },
+
+  subtract: (a, b) => {
+    res = BigInt(a) - BigInt(b);
+    console.log(res);
+    return res;
+  },
+
+  divide: (a, b) => {
+    if (b == 0) {
+      console.log("На ноль делить нельзя");
+      return "На ноль делить нельзя";
+    } else {
+      res = BigInt(a) / BigInt(b);
+      console.log(res);
+      return res;
+    }
+  },
+};
+alert(BigNumber.add(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
+alert(BigNumber.multiply(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
+alert(BigNumber.subtract(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
+alert(BigNumber.divide(Number.MAX_SAFE_INTEGER, 0));
+alert(BigNumber.divide(Number.MAX_SAFE_INTEGER, 2));
